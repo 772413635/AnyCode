4a13e15b827b3fdb23e8b0fbfd591ee20883bf0c
Isaac <xuhonggang@hotmail.com>
2016-03-29 17:52:41 +0800

    获取收获地址

--- SourceCode/AnyCode/Controllers/SeaFoodController.cs
+++ SourceCode/AnyCode/Controllers/SeaFoodController.cs
@@ -44,7 +44,7 @@ namespace AnyCode.Controllers
         {
             return new JsonpResult
             {
-                Data = null,
+                Data = new{},
                 JsonRequestBehavior = JsonRequestBehavior.AllowGet
             };
         }
